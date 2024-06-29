05880abc52b5b00ab2d43ab654827f3e4cb12355
azix khan <azixkhan.55@gmail.com>
2024-06-29 11:22:33 +0500

    Expense Tracker

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:expenses_tracker/app_view.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyAppView();
+  }
+}
---
+++ lib/app_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Expense Teacker",
+    );
+  }
+}
