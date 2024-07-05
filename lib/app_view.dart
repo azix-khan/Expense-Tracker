ad3a34ba49558f15fa8a76682e71bef2dfc2433e
azix khan <azixkhan.55@gmail.com>
2024-07-05 16:07:44 +0500

    working on theme

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -19,7 +19,7 @@ class MyAppView extends StatelessWidget {
           primary: const Color(0xFF00B2E7),
           secondary: const Color(0xFFE064F7),
           tertiary: const Color(0xFFFF8D6C),
-          outline: Colors.grey,
+          outline: Colors.grey[500],
         ),
       ),
       home: BlocProvider(
